463f78ba9465eaa84821eabf8ec78577a12e54c6
Paul <you@example.com>
2025-06-01 15:37:41 +0100

    build: another commit

--- packages/solid/src/stories/useRole.stories.tsx
+++ packages/solid/src/stories/useRole.stories.tsx
@@ -66,7 +66,7 @@ export function UseRole() {
             <FloatingPortal>
               <div
                 ref={floating.refs.setFloating}
-                style={floating.floatingStyles}
+                style={floating.floatingStyles()}
                 {...interactions.getFloatingProps()}
                 class="bg-gray-900 text-white px-3 py-2 rounded-lg shadow-lg text-sm max-w-xs z-50"
               >
